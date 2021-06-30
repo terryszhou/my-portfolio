a6f472ba1f748fd00f4f33bd9fc626c9b9681d7f
TERRYSZHOU <TERRYSZHOU@GMAIL.COM>
2021-06-30 15:15:11 -0500

    Added navbar component/rendered routes.

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { Home } from './components/Home'
 import { About } from './components/About'
 import { Projects } from './components/Projects'
 import { Contact } from './components/Contact'
+import { Nav } from './components/Nav'
 
 export const App = () => {
     return (
@@ -26,6 +27,7 @@ export const App = () => {
                 path="/contact"
                 component={Contact}
             />
+            <Nav />
         </BrowserRouter>
     )
 }
---
+++ src/components/Nav.jsx
@@ -0,0 +1,32 @@
+import { NavLink } from 'react-router-dom'
+
+export const Nav = () => {
+    return (
+        <div className="container">
+            <nav className="justify-content-center navbar navbar-expand navbar-light bg-light fixed-bottom">
+                <div class="navbar-nav">
+                    <div class="nav-item">
+                        <a href="/" class="nav-link">
+                            Home
+                        </a>
+                    </div>
+                    <div class="nav-item">
+                        <a href="/about" class="nav-link">
+                            About
+                        </a>
+                    </div>
+                    <div class="nav-item">
+                        <a href="/projects" class="nav-link">
+                            Projects
+                        </a>
+                    </div>
+                    <div class="nav-item">
+                        <a href="/contact" class="nav-link">
+                            Contact
+                        </a>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    )
+}
