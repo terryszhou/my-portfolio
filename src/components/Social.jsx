70d870d3bb516db3fadaf512cfeeb2358d8be361
TERRYSZHOU <TERRYSZHOU@GMAIL.COM>
2021-06-30 16:13:34 -0500

    Created social media sidebar

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -1,7 +1,18 @@
 export const Social = () => {
     return (
-        <div>
-            Hello from Social
+        <div class="social">
+            <a href="https://www.linkedin.com/in/terryszhou/" target="_blank" rel="noreferrer">
+                <div class="icon fab fa-linkedin"></div>
+            </a>
+            <a href="https://github.com/terryszhou" target="_blank" rel="noreferrer">
+                <div class="icon fab fa-github"></div>
+            </a>
+            <a href="https://codepen.io/terryszhou" target="_blank" rel="noreferrer">
+                <div class="icon fab fa-codepen"></div>
+            </a>
+            <a href="https://instagram.com/terry.s.zhou" target="_blank" rel="noreferrer">
+                <div class="icon fab fa-instagram"></div>
+            </a>
         </div>
     )
 }
