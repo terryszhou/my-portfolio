aba78c1a91740a7234070c8bce6229c87c9b08ff
TERRYSZHOU <TERRYSZHOU@GMAIL.COM>
2021-07-02 11:57:49 -0500

    Added contact form, no route.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,6 +2,20 @@ export const Contact = () => {
     return (
         <div className="container pt-3">
             <h2>Contact</h2>
+            <form id="contact-form">
+                <div class="form-group">
+                    <label for="name">Name</label>
+                    <input type="text" class="form-control" name="name"/>
+                </div>
+                <div class="form-group">
+                    <label for="email">Email</label>
+                    <input type="email" class="form-control" name="email"/>
+                </div>
+                <div class="form-group">
+                    <label for="message">Message</label>
+                    <textarea class="form-control" name="message"></textarea>
+                </div>
+            </form>
         </div>
     )
 }
