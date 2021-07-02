4068d1a69fa98a8d4212351787c552c3a54678a6
TERRYSZHOU <TERRYSZHOU@GMAIL.COM>
2021-07-02 12:16:10 -0500

    Added sample text to About component.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,6 +3,10 @@ export const About = () => {
         <div className="container pt-3">
             <h2>About</h2>
             <div className="about-wrapper pt-3">
+                <p class="about-text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vel nibh in turpis porta elementum in eleifend dolor. Sed condimentum nisi vitae ligula imperdiet, ac ullamcorper nibh tempus. Maecenas ornare, neque nec vulputate feugiat, leo turpis interdum erat, ac bibendum orci tellus ut augue. Vivamus egestas placerat venenatis. Cras hendrerit lacinia eleifend. Donec mattis est sit amet justo scelerisque malesuada. Suspendisse in magna non est gravida molestie et vitae nisl. Phasellus posuere pellentesque suscipit. Sed dictum egestas tortor non accumsan. Mauris nisi sapien, ultricies non pellentesque a, gravida vitae leo. Sed suscipit elementum quam a tincidunt.
+                    Praesent accumsan tellus metus, vitae ornare velit sollicitudin eu. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam mattis viverra pellentesque. Pellentesque id mattis tortor. Nunc finibus at urna eu condimentum. Donec ornare et lorem ac maximus. Nunc dolor odio, suscipit sed elementum nec, aliquet vel ipsum. Maecenas sit amet venenatis ante. Donec vehicula lectus id aliquam consequat.
+                </p>
                 <h3>My Toolbox</h3>
                 <div class="card">
                     <div class="table-responsive">
